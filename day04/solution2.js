492e6663f34dfa8548f15f7bdfc5841217c59686
Your Name <you@example.com>
2020-12-13 16:54:37 +0100

    fix: Day 4, Part 2/2

--- day04/solution2.js
+++ day04/solution2.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 // const filename = 'day04/demo.input.invalid'
 // const filename = 'day04/demo.input.valid'
-const filename = 'day04/demo.input.webarchive'
-// const filename = 'day04/input.webarchive'
+// const filename = 'day04/demo.input.webarchive'
+const filename = 'day04/input.webarchive'
 const file = fs.readFileSync(filename, 'utf8');
 console.log('OK:');
 const passports = file.split("\n\n");
@@ -10,8 +10,8 @@ const expected = [
     ["byr", /^((19[2-9][0-9])|([2]00[0-2]))$/],
     ["iyr", /^20((1[0-9])|(20))$/],
     ["eyr", /^20((2[0-9])|(30))$/],
-    ["hgt", /^(1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in)$/],
-    ["hcl", /^#([0-9]|[a-f]){6}$/],
+    ["hgt", /^((1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in))$/],
+    ["hcl", /^(#([0-9]|[a-f]){6})$/],
     ["ecl", /^((amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth))$/],
     ["pid", /^[0-9]{9}$/],
 ];
