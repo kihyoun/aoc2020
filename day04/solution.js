2e06cffa43a9037f8a52ddc9ed2b9d35a85bb2d3
Your Name <you@example.com>
2020-12-13 12:12:10 +0100

    Day 4, Part 1/2

--- day04/solution.js
+++ day04/solution.js
@@ -1,6 +1,32 @@
 const fs = require('fs');
-const filename = 'day04/demo.input.webarchive'
-// const filename = 'day04/input.webarchive'
+// const filename = 'day04/demo.input.webarchive'
+const filename = 'day04/input.webarchive'
 const file = fs.readFileSync(filename, 'utf8');
 console.log('OK:');
-const lines = file.split("\n");
+const passports = file.split("\n\n");
+const expected = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid"
+];
+
+let validPassports = 0;
+
+passports.forEach(passport => {
+    const fields = passport.split('\n').join(" ").split(' ');
+
+    if (expected.every(cap => {
+        return typeof fields.find(field => {
+            const [caption, value] = field.split(":");
+            return caption === cap;
+        }) !== "undefined";
+    })) {
+        validPassports++;
+    }
+});
+
+console.log(validPassports)
