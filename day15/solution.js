f5f4faec235f23e382b97285e4eef2ad100891ae
Ki-Hyoun Kim <kihyoun@gmail.com>
2021-01-18 21:03:21 +0100

    Day 15: add data

---
+++ day15/solution.js
@@ -0,0 +1,14 @@
+const input = "1,3,2" // the 2020th number spoken is 1.
+//const input = "2,1,3" // the 2020th number spoken is 10.
+//const input = "1,2,3" // the 2020th number spoken is 27.
+//const input = "2,3,1" // the 2020th number spoken is 78.
+//const input = "3,2,1" // the 2020th number spoken is 438.
+//const input = "3,1,2" // the 2020th number spoken is 1836.
+
+// const input = '0,13,16,17,1,10,6';
+
+
+
+for (let n = 0; n < 2020; n++) {
+
+}
